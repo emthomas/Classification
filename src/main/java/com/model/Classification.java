c43a5c105d6a4b26f9745746a1fc48e7cfeea2f0
thome127 <sebastien.thomas@disney.com>
2014-06-05 15:07:30 -0400

    Create dataset generator

--- src/main/java/com/model/Classification.java
+++ src/main/java/com/model/Classification.java
@@ -1,12 +1,14 @@
 package com.model;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 
+import weka.classifiers.Classifier;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.functions.LinearRegression;
 import weka.core.Instance;
@@ -14,43 +16,43 @@ import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Standardize;
 
-/**
- * Hello world!
- *
- */
 public class Classification 
 {
+	private static NaiveBayes model;
+	
     public static void main( String[] args ) throws Exception
     {
-    	String sampleFile = "data/house.arff";
-        BuildSampleDataset(sampleFile);
-		Instances data = new Instances(new BufferedReader(new FileReader(sampleFile)));
-		data.setClassIndex(data.numAttributes() - 1);
-		StandardizeFeatures(data);
-		//build model
-		//LinearRegression model = new LinearRegression();
-		NaiveBayes model = new NaiveBayes();
-		String[] options = {"-K"};
-		model.setOptions(options);
-		model.buildClassifier(data); //the last instance with missing class is not used
+    	String sampleFile = "data/guests.arff";
+		Instances dataTrain = BuildNaiveBayesSampleDataset(sampleFile);
+    	Instances dataTest = BuildNaiveBayesSampleDataset(sampleFile);
+    	setModel();
+		train(model, dataTrain);
+		System.out.println(dataTrain.toSummaryString());
 		System.out.println(model);
-		Instance test = null;
+		test(model, dataTest);
+    }
+    
+    public static void setModel() throws Exception {
+    	model = new NaiveBayes();
+    	String[] options = {"-K"};
+		model.setOptions(options);
+    }
+    
+    public static void train(Classifier model, Instances dataset) throws Exception {
+    	model.buildClassifier(dataset);
+    }
+    
+    public static void test(Classifier model, Instances dataset) throws Exception {
+    	Instance test = null;
 		double testClass = 0;
 		double predictionClass = 0;
 		int count = 0;
-		int total = data.numInstances();
+		int total = dataset.numInstances();
 		double tolerance = 0.05;
 		double percentDiff = 0;
 		
-//		test=data.lastInstance();
-//		System.out.println(test);
-//		
-//		for(double distribution : model.distributionForInstance(test)) {
-//			System.out.println(distribution);
-//		}
-		
 		for(int i=0; i<total; i++) {
-			test = data.instance(i);
+			test = dataset.instance(i);
 			testClass = test.classValue()+1;
 			predictionClass = model.classifyInstance(test)+1;
 			percentDiff = Math.abs(((testClass/predictionClass)-1));
@@ -61,10 +63,9 @@ public class Classification
 		
 		double accuracy = Math.round(100.0*count/total);
 		System.out.println("Accuracy: "+accuracy+"%");
-		
     }
     
-    public static void BuildSampleDataset (String fileName) throws Exception {
+    public static Instances BuildNaiveBayesSampleDataset (String fileName) throws Exception {
     	PrintStream ps = new PrintStream(new FileOutputStream(fileName));
     	String header = "@RELATION guest\n"+
     					"@ATTRIBUTE timeOnSite NUMERIC\n"+
@@ -74,12 +75,11 @@ public class Classification
     					"@ATTRIBUTE income NUMERIC\n"+
     					"@ATTRIBUTE children NUMERIC\n"+
     					"@ATTRIBUTE class {-1,+1}\n"+
-    					//"@ATTRIBUTE class NUMERIC\n"+
     					"@DATA";
     	ps.println(header);
     	double[] features = new double[6];
     	String booker = "";
-    	for(int i=0; i<10; i++) {
+    	for(int i=0; i<100; i++) {
     		for(int j=0; j<features.length; j++) {
     			
     			if(Math.rint(Math.random())==1) {
@@ -89,7 +89,7 @@ public class Classification
     				features[j]=Math.random();	
     			}
     		}
-    		booker = Math.rint(Math.random())==1?"+1":"-1";
+    		booker = Math.rint(Math.random()*10)==1?"+1":"-1";
     		for(int j=0; j<features.length; j++) {
     			if(booker.equals("+1")) {
     		//	System.out.print(features[j]+",");
@@ -104,14 +104,18 @@ public class Classification
     	ps.println(booker);
     	}
     	ps.close();
+    	
+    	Instances data = new Instances(new BufferedReader(new FileReader(fileName)));
+		data.setClassIndex(data.numAttributes() - 1);
+		StandardizeFeatures(data);
+		
+		return data;
     }
     
     public static void StandardizeFeatures(Instances features) throws Exception {
 		Standardize filter = new Standardize();
 		filter.setInputFormat(features);
 		Instances new_features = Filter.useFilter(features, filter);
-		System.out.println("BEFORE\n"+features);
 		features = new_features;
-		System.out.println("AFTER\n"+features);
 	}
 }
